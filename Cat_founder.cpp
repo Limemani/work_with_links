66df7ec8001fbe6cead0db9f9dc7114476efd5bc
Limemani <ivan.horror2018@yandex.ru>
2020-09-08 16:34:03 +0300

    Begin

---
+++ Cat_founder.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void UptadateIfGreater(int new_facts, int& distans) {
+    if (new_facts > distans) {
+        distans = new_facts;
+    }
+}
+
+int main()
+{
+    int max_distans = 26;
+    UptadateIfGreater(30, max_distans);
+    cout << max_distans;
+}
